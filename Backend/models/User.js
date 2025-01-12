83b8fb861d563e7f96ce00bebb5cbb5540cb1c19
AjayKumar-125 <ajaykumar.bscsf21kdk@iba-suk.edu.pk>
2025-01-13 03:22:31 +0500

    duplicate hashing functinality removed

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -31,17 +31,17 @@ const UserSchema = new mongoose.Schema(
 );
 
 // Pre-save middleware to hash passwords before storing
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+// UserSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) return next();
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 // Method to compare passwords during login
 UserSchema.methods.comparePassword = async function (password) {
